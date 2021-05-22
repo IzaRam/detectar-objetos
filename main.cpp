9e1f58bc1fcef8938196bc8eade8f340b4117c97
IzaRam <izadoraramos2@gmail.com>
2021-05-22 16:27:56 -0300

    Fix: main.cpp indentation

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace cv;
 
 int main(){
 
-    CvFont font;
-    double hScale=0.7;
-    double vScale=0.7;
-    int    lineWidth=1;
-    cvInitFont(&font,CV_FONT_HERSHEY_DUPLEX|CV_FONT_ITALIC, hScale,vScale,0,lineWidth);
+	CvFont font;
+	double hScale=0.7;
+	double vScale=0.7;
+	int    lineWidth=1;
+	cvInitFont(&font,CV_FONT_HERSHEY_DUPLEX|CV_FONT_ITALIC, hScale,vScale,0,lineWidth);
 
 	char path[30];
 
@@ -20,100 +20,100 @@ int main(){
 
 	cin>>path;
 
-    IplImage* img =  cvLoadImage(path);
+	IplImage* img =  cvLoadImage(path);
 
-    int width, height, nchannels, step, offset;
+	int width, height, nchannels, step, offset;
 
-    width = img->width;
-    height = img->height;
-    nchannels = img->nChannels;
-    step = img->widthStep;
+	width = img->width;
+	height = img->height;
+	nchannels = img->nChannels;
+	step = img->widthStep;
 
-    for(int i = 0 ; i < height ; i++){
-        uchar* data = (uchar*)(img->imageData + i*step);
-        for(int  j = 0 ; j < width ; j++){
+	for(int i = 0 ; i < height ; i++){
+		uchar* data = (uchar*)(img->imageData + i*step);
+		for(int  j = 0 ; j < width ; j++){
 
-            if(i==0){
-	            offset = j * nchannels;
-    	        data[offset] = 255;
-        	    data[offset+1] = 255;
-            	data[offset+2] = 255;
-            }
+			if(i==0){
+				offset = j * nchannels;
+				data[offset] = 255;
+				data[offset+1] = 255;
+				data[offset+2] = 255;
+			}
 
-            if(j==0){
-	            offset = j * nchannels;
-    	        data[offset] = 255;
-        	    data[offset+1] = 255;
-            	data[offset+2] = 255;
-            }
+			if(j==0){
+				offset = j * nchannels;
+				data[offset] = 255;
+				data[offset+1] = 255;
+				data[offset+2] = 255;
+			}
 
-            if(i==height-1){
-	            offset = j * nchannels;
-    	        data[offset] = 255;
-        	    data[offset+1] = 255;
-            	data[offset+2] = 255;
-            }
+			if(i==height-1){
+				offset = j * nchannels;
+				data[offset] = 255;
+				data[offset+1] = 255;
+				data[offset+2] = 255;
+			}
 
-            if(j==width-1){
-	            offset = j * nchannels;
-    	        data[offset] = 255;
-        	    data[offset+1] = 255;
-            	data[offset+2] = 255;
-            }
-        }
-    }
+			if(j==width-1){
+				offset = j * nchannels;
+				data[offset] = 255;
+				data[offset+1] = 255;
+				data[offset+2] = 255;
+			}
+		}
+	}
 
-    cvNamedWindow("Raw");
-    cvShowImage("Raw",img);
+	cvNamedWindow("Raw");
+	cvShowImage("Raw",img);
 
-    IplImage* imgGrayScale = cvCreateImage(cvGetSize(img), 8, 1);
-    cvCvtColor(img,imgGrayScale,CV_BGR2GRAY);
+	IplImage* imgGrayScale = cvCreateImage(cvGetSize(img), 8, 1);
+	cvCvtColor(img,imgGrayScale,CV_BGR2GRAY);
 
 	int threshold;
 	cout<<"Valor threshold: ";
 	cin>>threshold;
-    cvThreshold(imgGrayScale,imgGrayScale,threshold,255,1); //folha = 234, of = 100, gv = 150, c5 = 200, c4 = 55, c3 = 100
+	cvThreshold(imgGrayScale,imgGrayScale,threshold,255,1); //folha = 234, of = 100, gv = 150, c5 = 200, c4 = 55, c3 = 100
 
-    cvDilate(imgGrayScale,imgGrayScale);
-    cvErode(imgGrayScale,imgGrayScale);
+	cvDilate(imgGrayScale,imgGrayScale);
+	cvErode(imgGrayScale,imgGrayScale);
 
-    cvErode(imgGrayScale,imgGrayScale);
-    cvDilate(imgGrayScale,imgGrayScale);
+	cvErode(imgGrayScale,imgGrayScale);
+	cvDilate(imgGrayScale,imgGrayScale);
 
-    cvNamedWindow("Raw2");
-    cvShowImage("Raw2",imgGrayScale);
+	cvNamedWindow("Raw2");
+	cvShowImage("Raw2",imgGrayScale);
 
-    CvSeq* contours;
-    CvSeq* result;
-    CvMemStorage *storage = cvCreateMemStorage(0);
+	CvSeq* contours;
+	CvSeq* result;
+	CvMemStorage *storage = cvCreateMemStorage(0);
 
-    cvFindContours(imgGrayScale, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+	cvFindContours(imgGrayScale, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
 
-    int cont = 0;
-    char c[5];
+	int cont = 0;
+	char c[5];
 
-    while(contours){
+	while(contours){
 
-	    result = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0);
+		result = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0);
 
-    	double area = cvContourArea(result);
-    	cout<<"Area do objeto "<<cont<<": "<<area<<endl;
+		double area = cvContourArea(result);
+		cout<<"Area do objeto "<<cont<<": "<<area<<endl;
 
-	    CvPoint *pt[result->total];
-	    for(int i=0;i<result->total;i++){
-    	    pt[i] = (CvPoint*)cvGetSeqElem(result, i);
-	    }
+		CvPoint *pt[result->total];
+		for(int i=0;i<result->total;i++){
+			pt[i] = (CvPoint*)cvGetSeqElem(result, i);
+		}
 
-	    cvDrawContours(img, contours, Scalar(0, 0, 255), Scalar(0, 0, 255), 0, 2, 8);
-    	cont++;
-	    sprintf(c,"%i",cont);
+		cvDrawContours(img, contours, Scalar(0, 0, 255), Scalar(0, 0, 255), 0, 2, 8);
+		cont++;
+		sprintf(c,"%i",cont);
 
-    	const char* obj = c;
+		const char* obj = c;
 
-	    cvPutText (img,obj,*pt[result->total/2], &font, cvScalar(255,0,0));
+		cvPutText (img,obj,*pt[result->total/2], &font, cvScalar(255,0,0));
 
-    	contours = contours->h_next;
-    }
+		contours = contours->h_next;
+	}
 
 	cvNamedWindow("Tracked");
 	cvShowImage("Tracked",img);
@@ -125,5 +125,5 @@ int main(){
 	cvReleaseImage(&img);
 	cvReleaseImage(&imgGrayScale);
 
-    return 0;
+	return 0;
 }
